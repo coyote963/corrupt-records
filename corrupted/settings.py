c41812d9f69079060f2d7a4c541cfc04864adc91
c bird <john.luan@gmail.com>
2021-05-18 21:29:58 -0400

    adds the home page

--- corrupted/settings.py
+++ corrupted/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'corrupted.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["home/templates/home"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
